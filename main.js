5e6acfabbc8b6fe8f8e5838b499f83b7e86588be
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-08-29 10:38:06 -0400

    Add skeleton

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-// this script will run every 15 minutes
-// it should check the database for new entries
-// and log them to Slack
+var request = require("request");
+
+function getVar(name) {
+	if (process.env[name]) {
+		return process.env[name];
+	}
+	return undefined;
+}
+
+var webhookURL = getVar("SLACK_WEBHOOK_URL");
+
+var payload = {"text": "Hello"};
+
+request.post({
+	url: webhookURL,
+	form: {payload: JSON.stringify(payload)}
+},
+function(err,httpResponse,body) {
+	if (err) {
+		console.log("fail :(");
+		return;
+	}
+	console.log("success");
+});
